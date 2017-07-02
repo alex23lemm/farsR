2f2046f941afe7c4942b942f5a7277fc8ddec73a
Alex Lemm <alex23lemm@gmail.com>
2017-07-02 16:29:56 +0200

    Add tests for fars_map_state

--- tests/testthat/test-fars_map_state.R
+++ tests/testthat/test-fars_map_state.R
@@ -1,5 +1,9 @@
 context("fars_map_state")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fars_map_state works", {
+  setwd(system.file("extdata", package = "farsR"))
+
+  expect_null(fars_map_state(13, 2015))
+  expect_error(fars_map_state(15, 2015), "nothing to draw: all regions out of bounds")
+  expect_error(fars_map_state(63, 2013), "invalid STATE number: 63")
 })
